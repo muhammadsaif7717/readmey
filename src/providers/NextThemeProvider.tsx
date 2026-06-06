b9bfa92271e432fad1e8e94ae0f8292ef6301fa8
MD SAIF ISLAM <muhammadsaif7717@gmail.com>
2026-06-06 23:48:40 +0600

    modefied

--- src/providers/NextThemeProvider.tsx
+++ src/providers/NextThemeProvider.tsx
@@ -9,7 +9,7 @@ export default function NextThemeProvider({
   children: React.ReactNode;
 }) {
   return (
-    <ThemeProvider attribute="class" enableSystem defaultTheme="dark">
+    <ThemeProvider attribute="class" enableSystem defaultTheme="light">
       {children}
     </ThemeProvider>
   );
